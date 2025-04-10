beeb5af293929e1e2ecee00e4edd96d80a7963f1
Amartya Nair <nairamartya15@gmail.com>
2025-04-11 02:40:16 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,12 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const socket = io('https://chatverse-v1.onrender.com', {
-  transports: ['websocket'],
+const socket = io("https://chatverse-v1.onrender.com", {
+  transports: ["websocket"],
   withCredentials: true,
 });
+
+socket.on('connect', () => {
+    console.log('Socket connected with ID:', socket.id);
+  });
 
 export default socket;
