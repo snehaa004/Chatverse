ead1c22cb7b1975c188df4568c2461bcf01d0192
Amartya Nair <nairamartya15@gmail.com>
2025-04-11 02:49:41 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,11 +1,15 @@
 import { io } from "socket.io-client";
-
+import socket from '../socket';
 // ✅ Connect to deployed backend
 const socket = io("https://chatverse-v1.onrender.com/", {
   transports: ["websocket"], // Forces WebSocket over polling (good for Render)
   withCredentials: true // Needed if server uses credentials or cookies
 });
 
+useEffect(() => {
+    socket.emit("addUser", currentUser); // 'sub' must be present
+  }, []);
+  
 // ✅ Log on connection
 socket.on('connect', () => {
   console.log('✅ Socket connected:', socket.id);
