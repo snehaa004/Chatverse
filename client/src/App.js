3e8596abffb53a567d4781f38e6b7510b9d0ee0f
Amartya Nair <nairamartya15@gmail.com>
2025-04-11 03:11:39 +0530

    socket back again

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,23 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import Messenger from './components/Messenger';
 import AccountProvider from './context/AccountProvider';
 
-import { useEffect } from 'react';
+import socket from './socket';
+
+// Example: register user
+socket.emit('addUser', currentUser);
+
+// Example: send message
+socket.emit('sendMessage', {
+  senderId: userId,
+  receiverId: otherUserId,
+  text: message,
+});
+
+// Example: set preferred language
+socket.emit('setLanguage', {
+  userId: userId,
+  language: 'en',
+});
 
 
 function App() {
